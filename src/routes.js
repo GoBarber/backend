35390b1354e5c3069f1a6060b378fed427fd6081
Bernardo Henrique <bernardohrl.unb@gmail.com>
2020-06-29 19:33:28 -0300

    Install Linter
    Installed ESLint and Prettier. Also setted an .editoConfig.

--- src/routes.js
+++ src/routes.js
@@ -1,9 +1,9 @@
-import { Router }  from 'express';
+import { Router } from 'express';
 
 const routes = new Router();
 
-routes.get('/', (req, res) =>{ 
-    return res.json({ message: 'Hello Alou2' });
+routes.get('/', (req, res) => {
+  res.json({ message: 'Hello Alou2' });
 });
 
 export default routes;
