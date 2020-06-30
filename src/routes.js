581553780d258ac5a034bc01e01158cad059468d
Bernardo Henrique <bernardohrl.unb@gmail.com>
2020-06-29 21:22:27 -0300

    Creates Database -> Models Connection

--- src/routes.js
+++ src/routes.js
@@ -1,9 +1,16 @@
 import { Router } from 'express';
+import User from './app/models/User';
 
 const routes = new Router();
 
-routes.get('/', (req, res) => {
-  res.json({ message: 'Hello Alou2' });
+routes.get('/', async (req, res) => {
+  const user = await User.create({
+    name: 'Bernardo Henrique',
+    email: 'bernardo@gmail.com',
+    password_hash: '1203190318',
+  });
+
+  res.json(user);
 });
 
 export default routes;
