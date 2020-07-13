a446406abebd2e182b2afe13d9c823a7e1500d69
Bernardo Henrique <bernardohrl.unb@gmail.com>
2020-07-13 10:58:44 -0300

    Adds Basic Routes
    And one example middleware

--- src/routes/index.ts
+++ src/routes/index.ts
@@ -1,11 +1,68 @@
-import { Router } from 'express';
+import { Request, Response, Router, NextFunction } from 'express';
+
+import { uuid, isUuid } from 'uuidv4';
 
 const routes = Router();
 
-routes.get('/', (request, response) => {
-  // const { name, email } = request.body;
+const projects: Array<any> = [];
 
-  response.json({ message: '111aaa' });
+// Middlewares
+function validateProjectId(
+  request: Request,
+  response: Response,
+  next: NextFunction,
+) {
+  const { id } = request.params;
+
+  if (!isUuid(id))
+    return response.status(404).json({ error: 'Invalid Project ID.' });
+
+  return next();
+}
+
+routes.use('/projects/:id', validateProjectId);
+
+routes.get('/projects', (request, response) => {
+  const { title } = request.query;
+
+  const result = title
+    ? projects.filter(project => project.title.includes(title))
+    : projects;
+
+  response.json(result);
+});
+
+routes.post('/projects', (request, response) => {
+  const { title, owner } = request.body;
+  const project = { id: uuid(), title, owner };
+
+  projects.push(project);
+  return response.json(project);
+});
+
+routes.put('/projects/:id', (request, response) => {
+  const { id } = request.params;
+  const { title, owner } = request.body;
+  const projectIndex = projects.findIndex(project => project.id === id);
+
+  const project = {
+    id,
+    title,
+    owner,
+  };
+
+  projects[projectIndex] = project;
+
+  return response.json(project);
+});
+
+routes.delete('/projects/:id', (request, response) => {
+  const { id } = request.params;
+  const projectIndex = projects.findIndex(project => project.id === id);
+
+  projects.splice(projectIndex, 1);
+
+  return response.status(204).send();
 });
 
 export default routes;
