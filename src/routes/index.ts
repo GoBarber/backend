351cb04ead0d7bb89b1de6ef5ee1f9bf0f28e230
Bernardo Henrique <bernardohrl.unb@gmail.com>
2021-09-13 20:56:55 -0300

    Creates User's Post Method
    Adds routes and Service

--- src/routes/index.ts
+++ src/routes/index.ts
@@ -1,8 +1,10 @@
 import { Router } from 'express';
 import projectsRouter from './projects.routes';
+import usersRouter from './users.routes';
 
 const routes = Router();
 
 routes.use('/projects', projectsRouter);
+routes.use("/users", usersRouter);
 
 export default routes;
