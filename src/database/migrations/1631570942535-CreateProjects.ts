fc1c261987b0e1c9d17258e2f03cb2cf3ed3f583
Bernardo Henrique <bernardohrl.unb@gmail.com>
2021-09-13 19:31:59 -0300

    Creates 'Projects' Migration

---
+++ src/database/migrations/1631570942535-CreateProjects.ts
@@ -0,0 +1,50 @@
+import {MigrationInterface, QueryRunner, Table} from "typeorm";
+
+export class CreateProjects1631570942535 implements MigrationInterface {
+
+    public async up(queryRunner: QueryRunner): Promise<void> {
+        await queryRunner.createTable(
+          new Table({
+            name: "projects",
+            columns: [
+              {
+                name: "id",
+                type: "uuid",
+                isPrimary: true,
+                generationStrategy: "uuid"
+              },
+              {
+                name: "url",
+                type: "varchar",
+                isNullable: false
+              },
+              {
+                name: "name",
+                type: "varchar",
+                isNullable: false
+              },
+              {
+                name: "description",
+                type: "varchar",
+                isNullable: false
+              },
+              {
+                name: "created_at",
+                type: "timestamp",
+                default: "now()"
+              },
+              {
+                name: "updated_at",
+                type: "timestamp",
+                default: "now()"
+              }
+            ]
+          })
+        );
+      }
+    
+      public async down(queryRunner: QueryRunner): Promise<void> {
+        await queryRunner.dropTable("projects");
+      }
+
+}
